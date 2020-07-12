644c5fb73a91c847cd85d84af8c327bce74f5c38
mark-fogle <mark.fogle@theactigraph.com>
2020-07-12 11:32:02 -0500

    feat: Added module init

---
+++ gt3x/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["Gt3xFileReader","Gt3xLogReader","Gt3xHeader","Gt3xRawEvent","Activity3Payload"]
+from gt3x.Gt3xLogReader import Gt3xLogReader
+from gt3x.Gt3xFileReader import Gt3xFileReader 
+from gt3x.Gt3xHeader import Gt3xHeader
+from gt3x.Gt3xRawEvent import Gt3xRawEvent
+from gt3x.Activity3Payload import Activity3Payload
