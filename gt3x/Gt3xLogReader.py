c52e8b467610138920bd9d8a3af897295c4f9dd5
mark-fogle <mark.fogle@theactigraph.com>
2020-07-12 20:30:41 -0500

    fix: ensured that log reader will properly handle eof

--- gt3x/Gt3xLogReader.py
+++ gt3x/Gt3xLogReader.py
@@ -14,11 +14,20 @@ class Gt3xLogReader:
 
         Parameters: 
         source: IO stream for log.bin file data
-        
+
         """
         self.source = source
     
     def read_event(self):
-        header = gt3x.Gt3xHeader(self.source.read(8))
-        raw_event = gt3x.Gt3xRawEvent(header, self.source.read(header.payload_size), self.source.read(1))
+        headerBytes = self.source.read(8)
+        if len(headerBytes) != 8:
+            return None
+        header = gt3x.Gt3xHeader(headerBytes)
+        payloadBytes = self.source.read(header.payload_size)
+        if len(payloadBytes) != header.payload_size:
+            return None
+        checksum = self.source.read(1)
+        if not checksum:
+            return None
+        raw_event = gt3x.Gt3xRawEvent(header, payloadBytes, checksum)
         return raw_event
