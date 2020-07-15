dec214f710a689dddc6e9189d6ae52c1c09598b5
mark-fogle <mark.fogle@theactigraph.com>
2020-07-14 20:43:20 -0500

    refactor: moved activity hubkeeffljbit pack logic into separate class

--- gt3x/Activity3Payload.py
+++ gt3x/Activity3Payload.py
@@ -1,71 +1,11 @@
-import gt3x.AccelerationSample
+import gt3x.BitPackAcceleration
 
 class Activity3Payload:
     """
     Class for Activity 3 Payload
     """
 
-    def unpack_activity(self, source, timestamp:int, flipXY: bool):
-        """
-        Unpacks activity stored as sets of 3, 12-bit integers 
-
-        Parameters:
-        source (byte array): Activity payload bytes array
-        flipXY (bool): If true sample is extracted in Y,X,Z order. Otherwise X,Y,Z order.
-
-        Returns:
-        Generator which produces acceleration samples as Int16 values
-        
-        """
-        offset=0
-        index = -1
-        sample = [0,0,0]
-        
-        def get_next_byte():
-            nonlocal index
-            index+=1
-            if index == len(source):
-                return None
-            return source[index]
-        
-        while True:
-            for axis in range(0,3):
-                if (offset & 0x7) == 0:
-                    current = get_next_byte()
-                    if current is None:
-                        return
-                    
-                    shifter = ((current & 0xFF) << 4)
-                    offset += 8
-                    
-                    current = get_next_byte()
-                    if current is None:
-                        return
-                    
-                    shifter |= ((current & 0xF0) >> 4)
-                    offset += 4
-                else:
-                    shifter = ((current & 0x0F) << 8)
-                    offset +=4
-                    current = get_next_byte()
-                    if current is None:
-                        return
-                    
-                    shifter |= (current & 0xFF)
-                    offset += 8
-                
-                if 0 != (shifter & 0x0800):
-                    shifter |= 0xF000
-                
-                if shifter > 32767:
-                    shifter -= 65535
-                sample[axis] = shifter
-            if flipXY:
-                yield gt3x.AccelerationSample(timestamp, x=sample[1], y=sample[0], z=sample[2])
-            else:
-                yield gt3x.AccelerationSample(timestamp, x=sample[0], y=sample[1], z=sample[2])
-
     def __init__(self, payloadBytes, timestamp):
-        self.AccelerationSamples = self.unpack_activity(payloadBytes, timestamp, False)
+        self.AccelerationSamples = gt3x.BitPackAcceleration.unpack_activity(payloadBytes, timestamp, False)
 
     
---
+++ gt3x/BitPackAcceleration.py
@@ -0,0 +1,64 @@
+import gt3x.AccelerationSample
+
+class BitPackAcceleration:
+    
+    @staticmethod
+    def unpack_activity(source, timestamp:int, flipXY: bool):
+        """
+        Unpacks activity stored as sets of 3, 12-bit integers 
+
+        Parameters:
+        source (byte array): Activity payload bytes array
+        flipXY (bool): If true sample is extracted in Y,X,Z order. Otherwise X,Y,Z order.
+
+        Returns:
+        Generator which produces acceleration samples as Int16 values
+        
+        """
+        offset=0
+        index = -1
+        sample = [0,0,0]
+        
+        def get_next_byte():
+            nonlocal index
+            index+=1
+            if index == len(source):
+                return None
+            return source[index]
+        
+        while True:
+            for axis in range(0,3):
+                if (offset & 0x7) == 0:
+                    current = get_next_byte()
+                    if current is None:
+                        return
+                    
+                    shifter = ((current & 0xFF) << 4)
+                    offset += 8
+                    
+                    current = get_next_byte()
+                    if current is None:
+                        return
+                    
+                    shifter |= ((current & 0xF0) >> 4)
+                    offset += 4
+                else:
+                    shifter = ((current & 0x0F) << 8)
+                    offset +=4
+                    current = get_next_byte()
+                    if current is None:
+                        return
+                    
+                    shifter |= (current & 0xFF)
+                    offset += 8
+                
+                if 0 != (shifter & 0x0800):
+                    shifter |= 0xF000
+                
+                if shifter > 32767:
+                    shifter -= 65535
+                sample[axis] = shifter
+            if flipXY:
+                yield gt3x.AccelerationSample(timestamp, x=sample[1], y=sample[0], z=sample[2])
+            else:
+                yield gt3x.AccelerationSample(timestamp, x=sample[0], y=sample[1], z=sample[2]) 
