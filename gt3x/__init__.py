983f3c42f2cbbfb94fbcebbf6e8d4968a2354d66
mark-fogle <mark.fogle@theactigraph.com>
2020-07-14 20:47:32 -0500

    feat: updated module init

--- gt3x/__init__.py
+++ gt3x/__init__.py
@@ -1,8 +1,13 @@
-__all__ = ["Gt3xFileReader","Gt3xLogReader","Gt3xHeader","Gt3xRawEvent","Activity3Payload"]
+__all__ = ["Gt3xFileReader","Gt3xLogReader","Gt3xHeader","Gt3xRawEvent","Activity3Payload","Gt3xEventTypes"]
 from gt3x.Gt3xLogReader import Gt3xLogReader
 from gt3x.Gt3xFileReader import Gt3xFileReader 
 from gt3x.Gt3xHeader import Gt3xHeader
 from gt3x.Gt3xRawEvent import Gt3xRawEvent
 from gt3x.Activity3Payload import Activity3Payload
+from gt3x.Activity2Payload import Activity2Payload
+from gt3x.Activity1Payload import Activity1Payload
 from gt3x.Gt3xCalibratedReader import Gt3xCalibratedReader
 from gt3x.AccelerationSample import AccelerationSample
+from gt3x.Gt3xEventTypes import Gt3xEventTypes
+from gt3x.BitPackAcceleration import BitPackAcceleration
+from gt3x.Gt3xInfo import Gt3xInfo
