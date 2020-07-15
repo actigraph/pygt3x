ca8cef7dac66bd3ee90d13198b3b5fa6885601bd
mark-fogle <mark.fogle@theactigraph.com>
2020-07-14 20:46:25 -0500

    feat added ability to calibrate activity using calibration version 2

--- gt3x/Gt3xCalibratedReader.py
+++ gt3x/Gt3xCalibratedReader.py
@@ -1,6 +1,8 @@
 import gt3x.Gt3xFileReader
 import gt3x.Gt3xRawEvent
 import gt3x.AccelerationSample
+import gt3x.Gt3xEventTypes
+import numpy as np
 
 class Gt3xCalibratedReader:
     """
@@ -15,6 +17,9 @@ class Gt3xCalibratedReader:
         Returns info dictionary from source reader
         """
         return self.source.read_info()
+    
+    def read_calibration(self):
+        return self.source.read_calibration()
 
     def calibrate_acceleration(self, raw_event: gt3x.Gt3xRawEvent):
         """
@@ -24,10 +29,54 @@ class Gt3xCalibratedReader:
             raw_event (Gt3xRawEvent): Activity event to calibrate.
 
         """
-        payload = gt3x.Activity3Payload(raw_event.payload, raw_event.header.timestamp)
+        if gt3x.Gt3xEventTypes(raw_event.header.eventType) == gt3x.Gt3xEventTypes.Activity3:
+            payload = gt3x.Activity3Payload(raw_event.payload, raw_event.header.timestamp)
+        else:
+            payload = gt3x.Activity2Payload(raw_event.payload, raw_event.header.timestamp)
+
+        calibration = self.source.read_calibration()
         info = self.read_info()
-        accelScale = float(info["Acceleration Scale"])
-        raw_event.CalibratedAcceleration = [gt3x.AccelerationSample(raw_event.header.timestamp, x=sample.x/accelScale, y=sample.y/accelScale, z=sample.z/accelScale) for sample in payload.AccelerationSamples]
+
+        if calibration is None or calibration['isCalibrated'] == True:
+            accelScale = info.get_acceleration_scale()
+            raw_event.CalibratedAcceleration = [gt3x.AccelerationSample(raw_event.header.timestamp, x=sample.x/accelScale, y=sample.y/accelScale, z=sample.z/accelScale) for sample in payload.AccelerationSamples]
+        else:
+            sampleRate = info.get_sample_rate()
+            raw_event.CalibratedAcceleration = self.calibrate_v2(payload.AccelerationSamples, calibration, sampleRate)
+
+    def calibrate_v2(self, samples, calibration: dict, sampleRate: int):
+        offsetX = float(calibration[f'offsetX_{sampleRate}'])
+        offsetY = float(calibration[f'offsetY_{sampleRate}'])
+        offsetZ = float(calibration[f'offsetZ_{sampleRate}'])
+
+        sensitivityXX = float(calibration[f'sensitivityXX_{sampleRate}'])
+        sensitivityYY = float(calibration[f'sensitivityYY_{sampleRate}'])
+        sensitivityZZ = float(calibration[f'sensitivityZZ_{sampleRate}'])
+
+        sensitivityXY = float(calibration[f'sensitivityXY_{sampleRate}'])
+        sensitivityXZ = float(calibration[f'sensitivityXZ_{sampleRate}'])
+        sensitivityYZ = float(calibration[f'sensitivityYZ_{sampleRate}'])
+
+        s11 = (sensitivityXX * 0.01) ** -1.0
+        s12 = ((sensitivityXY * 0.01 + 250) ** -1.0) - 0.004
+        s13 = ((sensitivityXZ * 0.01 + 250) ** -1.0) - 0.004
+
+        s21 = ((sensitivityXY * 0.01 + 250) ** -1.0) - 0.004
+        s22 = (sensitivityYY * 0.01) ** -1.0
+        s23 = ((sensitivityYZ * 0.01 + 250) ** -1.0) - 0.004
+
+
+        s31 = ((sensitivityXZ * 0.01 + 250) ** -1.0) - 0.004
+        s32 = ((sensitivityYZ * 0.01 + 250) ** -1.0) - 0.004
+        s33 = (sensitivityZZ * 0.01) ** -1.0
+
+
+        O = np.array([[offsetX, offsetY, offsetZ]])
+        S = np.array([[s11,s21,s31],[s12,s22,s32],[s13,s23,s33]])
+        
+        for sample in samples:
+            calibratedSample = np.matmul(S, (np.array([[sample.x,sample.y,sample.z]]) - O).transpose()).transpose()
+            yield gt3x.AccelerationSample(sample.timestamp, calibratedSample[0][0], calibratedSample[0][1], calibratedSample[0][2])
 
     def read_events(self, num_rows: int = None):
         """
@@ -38,6 +87,8 @@ class Gt3xCalibratedReader:
 
         """
         for raw_event in self.source.read_events(num_rows):
+            if not gt3x.Gt3xEventTypes(raw_event.header.eventType) in [gt3x.Gt3xEventTypes.Activity, gt3x.Gt3xEventTypes.Activity2, gt3x.Gt3xEventTypes.Activity3]:
+                continue
             self.calibrate_acceleration(raw_event)
             yield raw_event
         
