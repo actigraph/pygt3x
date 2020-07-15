1ce1a2358cc2e78475a34bca12598d1ad7e6e9fe
mark-fogle <mark.fogle@theactigraph.com>
2020-07-14 20:44:33 -0500

    feat: added gt3x event types class

---
+++ gt3x/Gt3xEventTypes.py
@@ -0,0 +1,34 @@
+from enum import Enum, unique
+
+@unique
+class Gt3xEventTypes(Enum):
+    Activity = 0
+    AntPlus = 1
+    Battery = 2
+    Event = 3
+    HeartRateBpm = 4
+    Lux = 5
+    MetaData = 6
+    Tag = 7
+    Temperature = 8
+    Epoch = 9
+    DailySummary = 10
+    HeartRateAnt = 11
+    Epoch2 = 12
+    Capsense = 13
+    HeartRateBle = 14
+    Epoch3 = 15
+    Epoch4 = 16
+    Imu9dof = 17
+    Magnetometer = 18
+    FifoError = 19
+    FifoDump = 20
+    Params = 21
+    ImuAccelerometer = 22
+    Debug = 23
+    SensorSchema = 24
+    SensorData = 25
+    Activity2 = 26
+    Activity3 = 27
+    LongPress = 28
+    ButtonEvent = 29
