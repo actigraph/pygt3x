f18510c2c5ab98ff7c1715ef1a55532b58ffd778
mark-fogle <mark.fogle@theactigraph.com>
2020-07-23 16:50:59 -0500

    feat: Created Calibration V2 service

---
+++ gt3x/CalibrationV2Service.py
@@ -0,0 +1,44 @@
+import numpy as np
+import gt3x.AccelerationSample
+
+
+class CalibrationV2Service:
+    def __init__(self, calibration: dict, sample_rate: int):
+        self.offset_vector = np.array([[0, 0, 0]])
+        self.sensitivity_matrix = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        self.set_calibration(calibration, sample_rate)
+
+    def set_calibration(self, calibration: dict, sample_rate: int):
+        offset_x = float(calibration[f'offsetX_{sample_rate}'])
+        offset_y = float(calibration[f'offsetY_{sample_rate}'])
+        offset_z = float(calibration[f'offsetZ_{sample_rate}'])
+
+        sensitivity_xx = float(calibration[f'sensitivityXX_{sample_rate}'])
+        sensitivity_yy = float(calibration[f'sensitivityYY_{sample_rate}'])
+        sensitivity_zz = float(calibration[f'sensitivityZZ_{sample_rate}'])
+
+        sensitivity_xy = float(calibration[f'sensitivityXY_{sample_rate}'])
+        sensitivity_xz = float(calibration[f'sensitivityXZ_{sample_rate}'])
+        sensitivity_yz = float(calibration[f'sensitivityYZ_{sample_rate}'])
+
+        s11 = (sensitivity_xx * 0.01) ** -1.0
+        s12 = ((sensitivity_xy * 0.01 + 250) ** -1.0) - 0.004
+        s13 = ((sensitivity_xz * 0.01 + 250) ** -1.0) - 0.004
+
+        s21 = ((sensitivity_xy * 0.01 + 250) ** -1.0) - 0.004
+        s22 = (sensitivity_yy * 0.01) ** -1.0
+        s23 = ((sensitivity_yz * 0.01 + 250) ** -1.0) - 0.004
+
+        s31 = ((sensitivity_xz * 0.01 + 250) ** -1.0) - 0.004
+        s32 = ((sensitivity_yz * 0.01 + 250) ** -1.0) - 0.004
+        s33 = (sensitivity_zz * 0.01) ** -1.0
+
+        self.offset_vector = np.array([[offset_x, offset_y, offset_z]])
+        self.sensitivity_matrix = np.array([[s11, s21, s31], [s12, s22, s32], [s13, s23, s33]])
+
+    def calibrate_samples(self, samples):
+        for sample in samples:
+            calibrated_sample = np.matmul(self.sensitivity_matrix, (
+                        np.array([[sample.x, sample.y, sample.z]]) - self.offset_vector).transpose()).transpose()
+            yield gt3x.AccelerationSample(sample.timestamp, calibrated_sample[0][0], calibrated_sample[0][1],
+                                          calibrated_sample[0][2])
