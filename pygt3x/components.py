7d3b79a43fdecf8ace65fedb787ee859fa709f86
Ali Neishabouri <ali.neishabouri@theactigraph.com>
2022-10-18 19:01:14 +0200

    Read side (#24)
    * Read more metadata

--- pygt3x/components.py
+++ pygt3x/components.py
@@ -83,23 +83,30 @@ class RawEvent:
 class Info:
     """Metadata class."""
 
-    start_date: int
-    device_type: str
     acceleration_max: float
-    sample_rate: int
-    timezone: str
-    subject_name: str
-    battery_voltage: float
-    download_date: int
-    unexpected_resets: int
-    last_sample_time: int
     acceleration_min: float
     acceleration_scale: float
-    serial_numer: str
-    limb: str
+    age: float
+    battery_voltage: float
     board_revision: str
-    stop_date: int
+    device_type: str
+    dominance: str
+    download_date: int
     firmware: str
+    height: float
+    last_sample_time: int
+    limb: str
+    mass: float
+    race: str
+    sample_rate: int
+    serial_numer: str
+    sex: str
+    side: str
+    start_date: int
+    stop_date: int
+    subject_name: str
+    timezone: str
+    unexpected_resets: int
 
     def __init__(self, zip_file):
         """Parse info.txt and returns dictionary with key/value pairs."""
@@ -112,22 +119,31 @@ class Info:
                 # The format of TimeZone is this: "TimeZone: -04:00:00"
                 if len(values) == 2 or values[0] == "TimeZone":
                     output[values[0].strip()] = ":".join(values[1:]).strip()
-        self.start_date = int(output.get("Start Date", 0))
-        self.device_type = output.get("Device Type", None)
         self.acceleration_max = float(output.get("Acceleration Max", 0))
-        self.sample_rate = int(output.get("Sample Rate", 0))
-        self.timezone = output.get("TimeZone", None)
-        self.subject_name = output.get("Subject Name", None)
+        self.acceleration_min = float(output.get("Acceleration Min", 0))
+        self.acceleration_scale = float(output.get("Acceleration Scale", 0))
+        self.age = float(output["Age"]) if "Age" in output else None
         self.battery_voltage = float(
             output.get("Battery Voltage", "0").replace(",", ".")
         )
-        self.download_date = int(output.get("Download Date", 0))
-        self.unexpected_resets = output.get("Unexpected Resets", 0)
-        self.last_sample_time = int(output.get("Last Sample Time", 0))
-        self.acceleration_min = float(output.get("Acceleration Min", 0))
-        self.acceleration_scale = float(output.get("Acceleration Scale", 0))
-        self.serial_numer = output.get("Serial Number", None)
-        self.limb = output.get("Limb", None)
         self.board_revision = output.get("Board Revision", None)
-        self.stop_date = int(output.get("Stop Date", 0))
+        self.device_type = output.get("Device Type", None)
+        self.dominance = output.get("Dominance", None)
+        self.download_date = (
+            int(output["Download Date"]) if "Download Date" in output else None
+        )
         self.firmware = output.get("Firmware", None)
+        self.height = float(output["Height"]) if "Height" in output else None
+        self.last_sample_time = int(output.get("Last Sample Time", 0))
+        self.limb = output.get("Limb", None)
+        self.mass = float(output["Mass"]) if "Mass" in output else None
+        self.race = output.get("Race", None)
+        self.sample_rate = int(output.get("Sample Rate", 0))
+        self.serial_numer = output.get("Serial Number", None)
+        self.sex = output.get("Sex", None)
+        self.side = output.get("Side", None)
+        self.start_date = int(output.get("Start Date", 0))
+        self.stop_date = int(output.get("Stop Date", 0))
+        self.subject_name = output.get("Subject Name", None)
+        self.timezone = output.get("TimeZone", None)
+        self.unexpected_resets = output.get("Unexpected Resets", 0)
