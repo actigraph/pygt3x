549070c61c4fed42771a234258ce353a79b1a663
Ali Neishabouri <ali.neishabouri@theactigraph.com>
2023-02-17 11:51:54 +0100

    Fix checking for activation of idle sleep mode (#30)
    * Fix Idle Sleep mode enabled
    
    * Improve logging

--- tests/test_ism.py
+++ tests/test_ism.py
@@ -6,9 +6,11 @@ def test_ism_enabled(ism_enabled_file, ism_disabled_file):
     with FileReader(ism_enabled_file) as reader:
         calibrated = CalibratedReader(reader)
         df_enabled = calibrated.to_pandas()
+        assert reader.idle_sleep_mode_activated
     with FileReader(ism_disabled_file) as reader:
         calibrated = CalibratedReader(reader)
         df_disabled = calibrated.to_pandas()
+        assert not reader.idle_sleep_mode_activated
     assert (df_enabled.X.groupby(level=0).count() == 30).all()
     assert (
         df_enabled.loc[1616169537:1616169574, "X"]
